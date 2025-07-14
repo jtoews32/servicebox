1fc8bed78d7354f6b6c914ffabd00eec5a3cfe64
Jon T <jtoews32@gmail.com>
2025-07-14 17:30:16 -0400

    updates

---
+++ src/main/java/com/photoapp/servicebox/ServiceboxApplication.java
@@ -0,0 +1,13 @@
+package com.photoapp.servicebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/photoapp/servicebox/ServiceboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoapp.servicebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
