4d06ffc9c81b07d28a7431dada21e06be3b9966c
Jon T <jtoews32@gmail.com>
2025-07-16 16:53:21 -0400

    updates

--- src/main/java/com/photoapp/modules/files/FileController.java
+++ src/main/java/com/photoapp/modules/files/FileController.java
@@ -12,22 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FileController {
-
-
     @Autowired
     PhotoService photoService;
 
-
     @PostMapping("/upload")
     @CrossOrigin(origins = "http://localhost:3000") 
-    public String createResource(@RequestBody Base64PhotoFile data) {
-        // Logic to save the newResource to a database or perform other operations
-        // For demonstration, we'll just return the received resource
-
+    public String upload(@RequestBody Base64PhotoFile data) {
         System.out.println("Received file: " + data.payload());
         String base64Image = data.payload();
-
-        
 
         if (base64Image.startsWith("data:image/jpeg;base64,")) {
             String base64Data = base64Image.substring(base64Image.indexOf(",") + 1);
@@ -37,8 +29,35 @@ public class FileController {
             String str = new String(decodedBytes, StandardCharsets.UTF_8);
             System.out.println(str);
 
-            // Here you would typically save the decoded bytes to a file or database    
- System.out.println("Received File Name: " + data.name());
+            System.out.println("Received File Name: " + data.name());
+
+
+            photoService.addPhotoFile(data);
+
+            return "Image uploaded and processed successfully!";
+        } else {
+
+            System.out.println("Received name: " + data.name());
+            return "Invalid image format. Expected data:image/jpeg;base64,";
+        }
+    }
+
+
+    @PostMapping("/download")
+    @CrossOrigin(origins = "http://localhost:3000") 
+    public String download(@RequestBody Base64PhotoFile data) {
+        System.out.println("Received file: " + data.payload());
+        String base64Image = data.payload();
+
+        if (base64Image.startsWith("data:image/jpeg;base64,")) {
+            String base64Data = base64Image.substring(base64Image.indexOf(",") + 1);
+            byte[] decodedBytes = Base64.getDecoder().decode(base64Data);
+
+
+            String str = new String(decodedBytes, StandardCharsets.UTF_8);
+            System.out.println(str);
+
+            System.out.println("Received File Name: " + data.name());
 
 
             photoService.addPhotoFile(data);
